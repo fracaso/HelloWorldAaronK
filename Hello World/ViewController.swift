0287c389ff1a0101e836852ed42289d1af7aa172
Aaron Kinney <akinney@students.mchenry.edu>
2019-08-30 20:30:09 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Aaron Kinney on 8/30/19.
+//  Copyright © 2019 HarvardInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
